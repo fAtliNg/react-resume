8151c5f0d5b9b1cefac78a27c56eabf84e28b7f9
fAtliNg <korgeflus@mail.ru>
2017-11-04 16:29:51 +0300

    actions: function style es

--- src/actions/actions.jsx
+++ src/actions/actions.jsx
@@ -1,31 +1,28 @@
 // HOME PAGE
-export const fetchHomePageData = payload => {
-    return{
-        type: 'FETCH.HOMEPAGE.DATA',
-        payload: payload
-    }};
+export const fetchHomePageData = payload => ({
+    type: 'FETCH.HOMEPAGE.DATA',
+    payload: payload
+});
 
 export const clearHomePageData = () => ({
     type: 'CLEAR.HOMEPAGE.DATA'
 });
 
 // ABOUT ME PAGE
-export const fetchAboutMePageData = payload => {
-    return{
-        type: 'FETCH.ABOUTMEPAGE.DATA',
-        payload: payload
-    }};
+export const fetchAboutMePageData = payload => ({
+    type: 'FETCH.ABOUTMEPAGE.DATA',
+    payload: payload
+});
 
 export const clearAboutMePageData = () => ({
     type: 'CLEAR.ABOUTMEPAGE.DATA'
 });
 
 // SKILLS PAGE
-export const fetchSkillsPageData = payload => {
-    return{
+export const fetchSkillsPageData = payload => ({
         type: 'FETCH.SKILLSPAGE.DATA',
         payload: payload
-    }};
+});
 
 export const clearSkillsPageData = () => ({
     type: 'CLEAR.SKILLSPAGE.DATA'
